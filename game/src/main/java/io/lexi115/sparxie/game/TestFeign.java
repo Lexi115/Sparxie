db795745d90a4bbf89be6b5592bb0d568645234c
Alessio <83905118+Lexi115@users.noreply.github.com>
2026-08-12 21:02:21 +0200

    Docker support

--- game/src/main/java/io/lexi115/sparxie/game/TestFeign.java
+++ game/src/main/java/io/lexi115/sparxie/game/TestFeign.java
@@ -3,7 +3,7 @@ package io.lexi115.sparxie.game;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "test", url = "http://localhost:8082")
+@FeignClient(name = "test", url = "http://gacha:8080")
 public interface TestFeign {
     @GetMapping("/test")
     String test();
